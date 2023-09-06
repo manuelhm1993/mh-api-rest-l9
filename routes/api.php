993512e1d11149f9f8d36cf60b0b2f53225806bb
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-06 19:22:23 -0400

    Verbo GET, se comentó la ruta por defecto en api.php y se creó una ruta test para el verbo GET, se consume desde postman

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Ruta por defecto
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Verbos HTTP
+//
+// GET: devolver información
+Route::get('/test', function () {
+    return 'Hola Manuel';
 });
